e2c6102de2b1a99cfbbec70de57d212c7734fc00
smokeyshawn18 <shudarsanpoudel25@gmail.com>
2025-09-03 18:59:31 +0545

    added gd stats

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,23 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Raynyze" },
+    { name: "description", content: "Smart Genuine Feedback for your Resume" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg)] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Keep Track of Your Resume's Performance with Raynyze</h1>
+          <h2>Review your Submissions and Get Smart Feedback</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
